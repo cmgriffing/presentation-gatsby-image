7f98f2fa908f32c65b6ff1f36ef56af5353a15f3
Chris Griffing <chris.griffing@freshconsulting.com>
2019-09-12 18:13:43 -0700

    Latest demo build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: `/presentation-gatsby-image/demo/`,
+  pathPrefix: `/presentation-gatsby-image/demo`,
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
